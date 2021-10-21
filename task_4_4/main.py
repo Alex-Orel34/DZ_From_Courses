14d5cf839d275cbcb7d92d5ecf1c698107c9ecec
Alex-Orel34 <74289527+Alex-Orel34@users.noreply.github.com>
2021-10-21 19:08:47 +0300

    Update main.py

--- task_4_4/main.py
+++ task_4_4/main.py
@@ -1,9 +1,11 @@
 # First solution with using of 'for'
 
 ls = list(map(int, input().split()))
+a=ls[0]
 ls_result = []
 for i in range(1, len(ls)):
     ls_result.append(ls[i])
+ls_result.append(a)
 print(ls_result)
 
 # Second solution with using of 'while'
@@ -11,7 +13,9 @@ print(ls_result)
 ls1 = list(map(int, input().split()))
 ls_result1 = []
 i=1
+a=ls1[0]
 while i < len(ls1):
     ls_result1.append(ls1[i])
     i+=1
+ls_result1.append(a)
 print(ls_result1)
